f7822d9d5e4a2409c6f82919c630f85f6e651805
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-05-29 19:57:52 -0400

    commit

--- Lesson2/server/express.js
+++ Lesson2/server/express.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template'
 //import userRoutes from './routes/user.routes'
 //import authRoutes from './routes/auth.routes'
 
@@ -17,12 +18,12 @@ app.use(helmet())
 app.use(cors())
 //app.use('/', userRoutes)
 //app.use('/', authRoutes)
-/*
+
 app.get('/', (req, res) => {
 	res.status(200).send(Template())
 })
 
-
+/*
 app.use((err, req, res, next) => {
 	if (err.name === 'UnauthorizedError') {
 		res.status(401).json({"error" : err.name + ": " + err.message})
--- Lesson2/server/server.js
+++ Lesson2/server/server.js
@@ -3,9 +3,7 @@ import app from './express'
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, 
-                                    useCreateIndex: true, 
-                                    useUnifiedTopology: true })
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
