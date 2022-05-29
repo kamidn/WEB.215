db29d7e5688725663dbabdb17c57f78d504bb16f
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-05-29 18:59:48 -0400

    commit

--- Lesson2/server/express.js
+++ Lesson2/server/express.js
@@ -4,9 +4,8 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
-import { Template } from 'webpack'
-import userRoutes from './routes/user.routes'
-import authRoutes from './routes/auth.routes'
+//import userRoutes from './routes/user.routes'
+//import authRoutes from './routes/auth.routes'
 
 const app = express()
 
@@ -16,14 +15,14 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
-app.use('/', userRoutes)
-app.use('/', authRoutes)
-
+//app.use('/', userRoutes)
+//app.use('/', authRoutes)
+/*
 app.get('/', (req, res) => {
 	res.status(200).send(Template())
 })
 
-//
+
 app.use((err, req, res, next) => {
 	if (err.name === 'UnauthorizedError') {
 		res.status(401).json({"error" : err.name + ": " + err.message})
@@ -32,5 +31,5 @@ app.use((err, req, res, next) => {
 		console.log(err)
 	}
 })
-
+*/
 export default app
