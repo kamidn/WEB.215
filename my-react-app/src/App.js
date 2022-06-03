b29e0a221d4db17c7831b527fce7040a9a56b797
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-06-03 14:15:18 -0400

    commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,20 @@
-//import logo from './logo.svg';
-import { ReactComponent as Logo } from "./logo.svg";
+  import logo from './logo.svg';
+import Posts from "./components/Posts";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Logo style={{ height: 200 }} />
+        <div>
+          <img src="/koi_fish_md.jpg" />
+          <img src="/maple_tree_md.jpg "/>
+          <img src="/wind_chimes_md.jpg" />
+        </div>
         <h1>My React App - Duy Nguyen</h1>
+
       </header>
+      <Posts />
     </div>
   );
 }
