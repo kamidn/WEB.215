37e81f5b8ec8dceb52d3a0313e1f69e5a6d9650c
Duy Nguyen <duytrong.nguyen77@gmail.com>
2022-06-03 13:45:48 -0400

    commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { ReactComponent as Logo } from "./logo.svg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo style={{ height: 200 }} />
         <h1>My React App - Duy Nguyen</h1>
       </header>
     </div>
